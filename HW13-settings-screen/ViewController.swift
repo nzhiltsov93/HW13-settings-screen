688e87c17b7619a7c7bc9a52113b330712a19c4a
NikitaZhiltsov <nzhiltsov93@gmail.com>
2022-08-30 23:26:25 +0700

    Initial Commit

---
+++ HW13-settings-screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW13-settings-screen
+//
+//  Created by Nikita Zhiltsov on 30.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
